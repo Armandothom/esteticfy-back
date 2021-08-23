e6719e5da27804d797fecb3220aac21a9ecd33de
Armando <armandothomazini@gmail.com>
2021-08-23 19:55:29 -0300

    Adicionado classe SALÃO

--- routes/salao/index.js
+++ routes/salao/index.js
@@ -2,7 +2,27 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    let rowsGet = await fastify.salaoClass.getSalao();
+    let rowsGet = await fastify.salaoClass.getSaloes();
     return rowsGet;
+  })
+
+  fastify.post('/', async function (request, reply) {
+    let idFromPost = await fastify.salaoClass.salvarSalao(request.body);
+    return idFromPost;
+  })
+
+  fastify.get('/:id', async function (request, reply) {
+    let rowFromID = await fastify.salaoClass.getById(request.params.id);
+    return rowFromID;
+  })
+
+  fastify.post('/:id', async function (request, reply) {
+    let rowFromID = await fastify.salaoClass.updateSalao(request.body, request.params.id);
+    return rowFromID;
+  })
+
+  fastify.post('/delete', async function (request, reply) {
+    let rowFromID = await fastify.salaoClass.deleteSalao(request.body.id);
+    return rowFromID;
   })
 }
--- servicos/SalaoClass.js
+++ servicos/SalaoClass.js
@@ -9,12 +9,54 @@ class SalaoClass {
     this.db = db;
   }
 
-  async getSalao () {
+  async getSaloes () {
     const { rows } = await this.db.query(
-      'SELECT * FROM salao', [],
+      'SELECT * FROM salao WHERE isdeleted != true', [],
     )
     return rows
   }
+
+  async getById (id) {
+    const { rows } = await this.db.query(
+      'SELECT  * FROM salao WHERE id = $1', [id],
+    )
+    return rows[0]
+  }
+
+  async salvarSalao(salao) {
+    const formData = [
+      salao.nome ? salao.nome : null,
+      salao.endereco ? salao.endereco : null,
+      false
+    ];
+
+    const id = await this.db.query(
+      format(
+        `INSERT INTO salao(
+          nome,
+          endereco,
+          isdeleted
+            )  VALUES %L RETURNING id`,
+        [formData]
+      )
+    );
+    return id;
+  }
+
+  async updateSalao(salao, id) {
+    const { text, values } = update("salao", salao
+    , { id: id });
+    const { rows } = await this.db.query(text, values);
+    return id;
+  }
+
+  async deleteSalao(id) {
+    const { text, values } = update("salao", {isdeleted : true}
+    , { id: id });
+    const { rows } = await this.db.query(text, values);
+    return id;
+  }
+
 
 }
 
