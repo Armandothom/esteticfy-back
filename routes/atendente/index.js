01eb46563019662ebc347465163261518797cc27
Armando <armandothomazini@gmail.com>
2021-08-25 19:39:21 -0300

    Adicionado inserir logs em atendentes

--- routes/atendente/index.js
+++ routes/atendente/index.js
@@ -9,6 +9,7 @@ module.exports = async function (fastify, opts) {
 
   fastify.post('/', async function (request, reply) {
     let idFromPost = await fastify.atendenteClass.salvarAtendente(request.body);
+    await fastify.historicoClass.inserirLog(2, 2, fastify.salao_id, fastify.user_id, fastify.user_id);
     return idFromPost;
   })
 
@@ -19,11 +20,13 @@ module.exports = async function (fastify, opts) {
 
   fastify.post('/:id', async function (request, reply) {
     let rowFromID = await fastify.atendenteClass.updateAtendente(request.body, request.params.id);
+    await fastify.historicoClass.inserirLog(2, 1, fastify.salao_id, fastify.user_id, fastify.user_id);
     return rowFromID;
   })
 
   fastify.post('/delete', async function (request, reply) {
     let rowFromID = await fastify.atendenteClass.deleteAtendente(request.body.id);
+    await fastify.historicoClass.inserirLog(2, 3, fastify.salao_id, fastify.user_id, fastify.user_id);
     return rowFromID;
   })
 
