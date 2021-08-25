b1f4bf9fd8b2401056ed50ae4df00756d6207112
Armando <armandothomazini@gmail.com>
2021-08-25 19:45:53 -0300

    Adicionado logs em vendas

--- routes/venda/index.js
+++ routes/venda/index.js
@@ -10,6 +10,7 @@ module.exports = async function (fastify, opts) {
 
   fastify.post('/', async function (request, reply) {
     let idFromPost = await fastify.vendaClass.salvarVenda(request.body, fastify.salao_id);
+    await fastify.historicoClass.inserirLog(7, 2, fastify.salao_id, fastify.user_id, fastify.user_id);
     return idFromPost;
   })
 
@@ -20,11 +21,13 @@ module.exports = async function (fastify, opts) {
 
   fastify.post('/:id', async function (request, reply) {
     let rowFromID = await fastify.vendaClass.updateVenda(request.body, request.params.id);
+    await fastify.historicoClass.inserirLog(7, 1, fastify.salao_id, fastify.user_id, fastify.user_id);
     return rowFromID;
   })
 
   fastify.post('/delete', async function (request, reply) {
     let rowFromID = await fastify.vendaClass.deleteVenda(request.body.id);
+    await fastify.historicoClass.inserirLog(7, 3, fastify.salao_id, fastify.user_id, fastify.user_id);
     return rowFromID;
   })
 
